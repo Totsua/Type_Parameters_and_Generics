8a3c7917d8ed88da013e1ed8c994585fffcab46f
Totsua <shamar098@gmail.com>
2024-10-25 11:52:23 +0100

    feat: added boxNames arrayList to Box

--- Type_Parameters_and_Generics/src/main/java/org/example/Box.java
+++ Type_Parameters_and_Generics/src/main/java/org/example/Box.java
@@ -1,10 +1,24 @@
 package org.example;
 
 import java.awt.geom.Arc2D;
+import java.util.ArrayList;
 
 public class Box<T, V extends Double> implements BoxOperations{
     private T genericAttribute;
     private V largestValue;
+    private String name;
+
+    // Static list of all box names
+    public static ArrayList<String> boxNames= new ArrayList<>();
+
+    public Box(T genericAttribute, V largestValue, String name) {
+        this.genericAttribute = genericAttribute;
+        this.largestValue = largestValue;
+        this.name = name;
+
+        // Everytime a Box object is created we'll keep track of all the names
+        boxNames.add(name);
+    }
 
     // Getters and Setters
     protected T getGenericAttribute() {
@@ -19,7 +33,7 @@ public class Box<T, V extends Double> implements BoxOperations{
 
     public void setLargestValue (V value1,V value2) {
 
-        // Compare the two values as "double", sets the largest value as a V type
+        // Compare the two values as "doubadded logic to setLargestValue in Boxle", sets the largest value as a V type
         if(value1.doubleValue() > value2.doubleValue()){
             this.largestValue = value1;
         }
