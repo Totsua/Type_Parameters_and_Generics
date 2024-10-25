e7ff0570a03309315854cc8eae02600d7496e6e3
Totsua <shamar098@gmail.com>
2024-10-25 10:39:41 +0100

    feat: added attributes/getters/setters to Box

--- Type_Parameters_and_Generics/src/main/java/org/example/Box.java
+++ Type_Parameters_and_Generics/src/main/java/org/example/Box.java
@@ -1,6 +1,22 @@
 package org.example;
 
-public class Box {
+public class Box<T, V> {
+    private T genericAttribute;
+    private V largestValue;
+
+    // Getters and Setters
+    protected T getGenericAttribute() {
+        return genericAttribute;
+    }
+
+    protected V getLargestValue() {
+        return largestValue;
+    }
+
+    protected void setGenericAttribute(T genericAttribute) {
+        this.genericAttribute = genericAttribute;
+    };
+
 
 
 }
